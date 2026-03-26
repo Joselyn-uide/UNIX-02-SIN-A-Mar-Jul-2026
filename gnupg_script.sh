146c14a6cc931d76ac9f354f6cc75a52072ebd79
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-03-26 12:43:04 +0000

    exportsecretkeys

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,3 +5,4 @@ gpg --full-generate-key #genero la llave publica y privada
 gpg --list-keys #exportar el llavero
 gpg --armor --export #exportar la llave pública
 gpg --list-secret-keys --keyid-format=long  #lista las llaves privadas
+gpg --armor --export-secret-keys XXXXXXXX #exporta la llave privada en formato de texto ASCII (las X representan el hash)
