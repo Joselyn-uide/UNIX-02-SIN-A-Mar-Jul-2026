93f86c047f4ca315fd7850c45f0b589f71025b54
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-04-02 12:51:08 +0000

    Install parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,10 @@
 cd / 
 #List the contents of the current directory by adding a symbol to the end of each name to identify what type of file it is.
 ls -F
+
+#Update the list of available packages and their versions from the repositories
+sudo apt update
+#Download and install the latest versions of all the packages you already have installed on your system
+sudo apt upgrade
+#Download and install the Parted tool specifically
+sudo apt install parted
