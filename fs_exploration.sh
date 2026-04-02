946dd233b0fe498598d991e9135daba347ebd058
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-04-02 12:29:25 +0000

    file type

---
+++ fs_exploration.sh
@@ -0,0 +1,4 @@
+#Change the current directory to the system root directory
+cd / 
+#List the contents of the current directory by adding a symbol to the end of each name to identify what type of file it is.
+ls -F
