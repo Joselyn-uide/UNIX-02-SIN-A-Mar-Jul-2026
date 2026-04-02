f08d0f459c6eac4d0a75230100f7797b45770d72
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-04-02 13:10:53 +0000

    information about the disks and their partitions

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -9,3 +9,15 @@ sudo apt update
 sudo apt upgrade
 #Download and install the Parted tool specifically
 sudo apt install parted
+
+#It displays detailed information about the disks and their partitions.
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" 
+#List all partitions on all disks with technical details.
+sudo parted -l
+#List the disks and partitions, detailing their format type (ext4, ntfs, fat32), unique identifiers, and where they are mounted.
+lsbk -f
+#&&: It is a logical connective that says: "if the previous command was successful, execute the next one."
+#Filters the list of all mounted file systems
+mount | grep 
+# echo -e, the -e parameter enables the interpretation of special characters with backslashes, allowing the use of commands like \n for line breaks
+-e 
