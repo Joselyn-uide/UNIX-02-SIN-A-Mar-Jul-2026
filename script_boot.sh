3050c7d7e2d44dc3b6fb8e19286e72e01f8c0136
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-04-09 12:26:00 +0000

    Relative and absolute path

---
+++ script_boot.sh
@@ -0,0 +1,6 @@
+#Create a new folder (directory) called "project"
+mkdir proyecto
+# The command cd project/ allows you to enter the "project" folder, and a relative path is one that defines a location based on your current directory rather than starting from the system root.
+cd proyecto/
+#The command moves you directly to that specific folder, and an absolute path is the complete and exact address of a file or folder from the system root (the origin /), regardless of which directory you are working in.
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
