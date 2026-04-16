486d287f4735ef629c90b2723900ef21c0c5b31c
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-04-16 12:56:13 +0000

    Directory management commands

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -2,3 +2,8 @@
 #The ls -lah command is used to list all the contents of a directory, including hidden files (-a), displaying technical details such as permissions and dates in long list format (-l), and transforming file sizes into readable units such as KB or MB (-h) for easier reading.
 ls -l -a -h
 ls -lah
+
+#Create a directory whose name is literally -rf (the use of -- tells Linux that what follows is not an option or parameter, but the name of the folder).
+mkdir -- -rf
+#Delete an empty directory that is named exactly -rf.
+rmdir -- -rf
