126f83ee66083667d4aa9de90a08386813663651
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-04-16 12:48:39 +0000

    Detailed and complete list of files

---
+++ boot_exploration.sh
@@ -0,0 +1,4 @@
+#Anatomy of a UNIX command  ---  command [options] [arguments]
+#The ls -lah command is used to list all the contents of a directory, including hidden files (-a), displaying technical details such as permissions and dates in long list format (-l), and transforming file sizes into readable units such as KB or MB (-h) for easier reading.
+ls -l -a -h
+ls -lah
