38e4e47ef3beeb0c176d2c89d9851fb2e42f5e05
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-04-16 13:10:57 +0000

    Help commands and manuals

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -7,3 +7,12 @@ ls -lah
 mkdir -- -rf
 #Delete an empty directory that is named exactly -rf.
 rmdir -- -rf
+
+#It shows a quick and summarized guide to all the options and parameters you can use with the ls command (like the ones we saw for -lah).
+ls --help
+#Open the official and detailed manual for the ls command, explaining in detail each function, permissions, and output formats.
+man ls
+#Displays the main Git manual, which serves as an index to understand how repositories work and how to access help for specific subcommands (such as man git-commit).
+man git
+#To navigate efficiently in the manual (man), use the / key followed by the term you wish to locate; once the search has started, press n to jump to the next match forward or N to return to the previous one in the opposite direction.
+man git-clone #We are looking for what --depth does:Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
