2524459c8c1f262476c962fafd696a28e6e0117c
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-04-26 18:23:39 +0000

    wip: Exploring Files in Linux (the ls command)

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,6 @@
+#BASIC COMMAND SYNTAX
+#Lists the files and directories within the current location to view the folder's contents.
+ls
+
+#This command doesn't exist (the system doesn't recognize it), since Linux commands are case-sensitive.
+LS
