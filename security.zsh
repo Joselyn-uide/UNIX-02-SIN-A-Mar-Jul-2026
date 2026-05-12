58077738e1ffe08c34e0487f3c4a3a9dfde03d4e
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-05-12 17:23:00 +0000

    Creating users and transferring file ownership

--- security.zsh
+++ security.zsh
@@ -38,3 +38,19 @@ ls -l
 
 #UMASK
 # The umask (user mask) is a system command and value that determines the default permissions assigned to any file or directory at the time of its creation. It acts as a filter that "subtracts" or masks permissions from a predefined base (usually 666 for files and 777 for directories), allowing users to automatically control the privacy of their new data without having to manually modify each item. In short, the higher the mask number, the more restrictive the resulting permissions will be for the group and other users.
+
+# `chown` (change owner) and `chgrp` (change group) are used to manage the ownership of files and directories. `chown` allows you to change the user who owns an item, which is essential for transferring control of a file to another person; it's also a versatile command that can simultaneously change the group. `chgrp`, on the other hand, specializes exclusively in modifying the owning group. Both commands are essential administration tools for defining who the legal "owner" of the data is and, therefore, to whom the previously defined permission rules (read, write, and execute) will apply.
+# whoami: This command returns the name of the user you are currently working as. In your case, it returned root, which is the superuser with full control over the system.
+whoami
+# echo "Hello" > my_file: Creates a new file called my_file with the text "Hello" inside. Since it was created by root, it initially belongs to them.
+echo "Hello" > my_file
+# ls -l my_file: Displays the file details. Here you confirmed that the owner was root and the group was also root (-rw-r--r-- 1 root root).
+ls -l my_file
+# useradd -m -s /usr/bin/zsh luna: This is an administrative command that creates a new user on the system called luna.
+useradd -m -s /usr/bin/zsh luna
+#- The -m parameter automatically creates their home directory (/home/luna).
+#- The -m parameter automatically creates their home directory (/home/luna). The `-s /usr/bin/zsh` parameter assigns the Zsh shell as your default command interpreter.
+#`chown luna my_file`: Here you used the `Change Owner` command. You changed the file's ownership so that the owner is now the user `luna`, removing ownership from `root`.
+`chown luna my_file`
+#`ls -l my_file`: You listed the file again to verify the change. The final output shows `-rw-r--r-- 1 luna root`, confirming that `luna` is now the owner, although the group remains `root`.
+`ls -l my_file`
