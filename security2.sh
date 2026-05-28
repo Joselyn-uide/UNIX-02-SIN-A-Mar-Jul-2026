90affd4b50a2fef9eb3e2a0fcb62d9b272cabd67
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-05-28 12:55:28 +0000

    wip: Identification and Creation of Default Files

--- security2.sh
+++ security2.sh
@@ -1,4 +1,4 @@
-#View current main group 
+# View current main group 
 id
 
 #only the main group name
@@ -7,3 +7,10 @@ id -gn
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 # The group is the user's primary group
+
+# View the current group
+id -gn
+echo "Grupo actual: $(id -gn)"
+# Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
