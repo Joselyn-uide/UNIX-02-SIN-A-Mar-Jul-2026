8fdee902b7682d58b1e3bdab6e4316926bed20ad
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-06-05 17:16:43 +0000

    wip: Control Operators, Command Grouping, and Flow Redirection in Bash

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -5,3 +5,24 @@ top&  # # Runs the 'top' process monitor in the background, freeing up the termi
 #Immediately force the closure of the process with the PID
 kill -9 (PID)
 kill -9 8414
+
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "The lzl command has failed"
+
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye World!" > output.txt
+cat output.txt
+echo "Bye Bye" >> output.txt
+cat output.txt
+
+ls -l / &> stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
+lzl 2> error.txt
+cat error.txt
