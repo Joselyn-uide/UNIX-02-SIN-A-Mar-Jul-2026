9f28284a17f79e645fd4ded97a60d87bfb5cc934
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-06-09 16:50:51 +0000

    wip: Argument Iteration and Evaluation Loop

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -8,3 +8,8 @@ ping -c 5 "${TARGET}"
 
 echo "The arguments are: $@"
 echo "The total number of arguments are: $#"
+
+# Change "$@" to "$*" to observe behavior. 
+for args in "$@"; do 
+    echo "${args}" 
+done
