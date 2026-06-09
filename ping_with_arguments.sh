5e142078389f37a9c24e7fa197dbdb8c37847d11
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-06-09 16:55:41 +0000

    wip: Keyboard Input and Variable Assignment Script

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -13,3 +13,12 @@ echo "The total number of arguments are: $#"
 for args in "$@"; do 
     echo "${args}" 
 done
+
+# Takes input from the user and assigns it to variables 
+echo "What is your first name?" 
+read -r firstname 
+
+echo "What is your last name?" 
+read -r lastname 
+
+echo "Your first name is ${firstname} and your last name is ${lastname}"
